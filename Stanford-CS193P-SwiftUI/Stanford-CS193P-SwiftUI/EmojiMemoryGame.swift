1198d31792e4241808903dd8e6468f9eac5d2cac
ranmyfriend <ranjithkumar2010a@live.com>
2020-08-07 20:35:56 +0530

    Stanford Lecture 2 Assignment Added and it is all about MVVM

---
+++ Stanford-CS193P-SwiftUI/Stanford-CS193P-SwiftUI/EmojiMemoryGame.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiMemoryGame.swift
+//  Stanford-CS193P-SwiftUI
+//
+//  Created by Ranjit on 07/08/20.
+//  Copyright © 2020 Ranjit. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        let emojis = ["👻", "🎃","🕷"]
+        return MemoryGame<String>(numberOfPairOfCards: emojis.count) { pairIndex in emojis[pairIndex]
+        }
+    }
+    
+    // MARK: - Access to the Model
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    
+    // MARK: - Intent(s)
+    
+    func choose(card: MemoryGame<String>.Card) {
+        model.choose(card: card)
+    }
+}
---
+++ Stanford-CS193P-SwiftUI/Stanford-CS193P-SwiftUI/MemoryGame.swift
@@ -0,0 +1,33 @@
+//
+//  MemoryGame.swift
+//  Stanford-CS193P-SwiftUI
+//
+//  Created by Ranjit on 07/08/20.
+//  Copyright © 2020 Ranjit. All rights reserved.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    var cards: Array<Card>
+    
+    func choose(card: Card) {
+        print("card chosen: \(card)")
+    }
+    
+    init(numberOfPairOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = Array<Card>()
+        for pairIndex in 0..<numberOfPairOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id: pairIndex*2))
+            cards.append(Card(content: content, id: pairIndex*2+1))
+        }
+    }
+    
+    struct Card: Identifiable {
+        var isFaceUp: Bool = true
+        var isMatched: Bool = false
+        var content: CardContent
+        var id: Int
+    }
+}
