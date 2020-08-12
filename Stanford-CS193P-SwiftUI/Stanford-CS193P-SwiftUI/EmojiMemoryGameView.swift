ff43e72c0edcefbd3341a0712b00f44d4e4df0ed
ranmyfriend <ranjithkumar2010a@live.com>
2020-08-12 21:56:41 +0530

    Simple MVVM Pattern Introduced

--- Stanford-CS193P-SwiftUI/Stanford-CS193P-SwiftUI/EmojiMemoryGameView.swift
+++ Stanford-CS193P-SwiftUI/Stanford-CS193P-SwiftUI/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  Stanford-CS193P-SwiftUI
 //
 //  Created by Ranjit on 04/08/20.
@@ -8,8 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var viewModel: EmojiMemoryGame
+//MVVM Pattern Followed here
+//Characterstics are below
+//Model => MemoryGame
+//View => EmojiMemoryGameView
+//ViewModel => EmojiMemoryGame
+
+struct EmojiMemoryGameView: View {
+   @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
         HStack {
@@ -44,6 +50,6 @@ struct CardView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(viewModel: EmojiMemoryGame())
+        EmojiMemoryGameView(viewModel: EmojiMemoryGame())
     }
 }
