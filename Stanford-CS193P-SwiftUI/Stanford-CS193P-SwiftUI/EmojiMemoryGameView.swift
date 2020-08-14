9498d8650abe4bdd79248238b7eaaffbbf23e4a4
ranmyfriend <ranjithkumar2010a@live.com>
2020-08-14 23:06:21 +0530

    Lecture 3: Reactive UI + Protocols + Layout Watched and demo code applied

--- Stanford-CS193P-SwiftUI/Stanford-CS193P-SwiftUI/EmojiMemoryGameView.swift
+++ Stanford-CS193P-SwiftUI/Stanford-CS193P-SwiftUI/EmojiMemoryGameView.swift
@@ -15,7 +15,7 @@ import SwiftUI
 //ViewModel => EmojiMemoryGame
 
 struct EmojiMemoryGameView: View {
-   @ObservedObject var viewModel: EmojiMemoryGame
+    @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
         HStack {
@@ -28,7 +28,6 @@ struct EmojiMemoryGameView: View {
         }
         .padding()
         .foregroundColor(Color.orange)
-        .font(.largeTitle)
     }
 }
 
@@ -36,15 +35,30 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            self.body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             }else {
-                RoundedRectangle(cornerRadius: 10).fill()
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
         }
+        .font(Font.system(size: fontSize(for: size)))
+    }    
+    
+    // MARK: - Drawing Constants
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3.0
+    func fontSize(for size: CGSize) -> CGFloat {
+        min(size.width,size.height) * 0.75
     }
 }
 
